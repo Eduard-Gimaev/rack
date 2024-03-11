ad93f604ebf012362be8543cae94524a294b9906
GED <gimaev.ufa@gmail.com>
2024-03-11 12:33:47 +0500

    feat:  add runtime

---
+++ app.rb
@@ -0,0 +1,24 @@
+class App
+    def call(env)
+      perform_request
+      [status, header, body]
+    end
+
+    private
+
+    def perform_request
+      sleep rand(2..3)
+    end
+
+    def status
+      200
+    end
+
+    def header
+      {'Content-Type' => 'text/plain'}
+    end
+
+    def body
+      ["Welcome abroad!\n"]
+    end
+end
