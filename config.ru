abb3f75068f205cbf4c7eea4eac59ee3dcc0e14a
GED <gimaev.ufa@gmail.com>
2024-05-13 12:51:27 +0500

    feat: added  time request by format

--- config.ru
+++ config.ru
@@ -1,7 +1,5 @@
-require_relative 'middleware/runtime'
-require_relative 'middleware/logger'
+require_relative 'middleware/time_request'
 require_relative 'app'
 
-use Runtime
-use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
+use TimeRequest
 run App.new
---
+++ middleware/time_request.rb
@@ -0,0 +1,34 @@
+require './time_formatter'
+
+class TimeRequest
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    req = Rack::Request.new(env)
+    create_response(req)
+  end
+
+  private
+  def create_response(request)
+    return response(404, 'Not Found') if request.path != '/time' || !request.params.key?('format')
+
+    formatter = TimeFormatter.new(request.params)
+    formatter.check_permit
+
+    if formatter.permitted?
+      response(200, formatter.success_message)
+    else
+      response(400, formatter.error_message)
+    end
+  end
+
+  def response(status, body)
+    res = Rack::Response.new
+    res.status = status
+    res.header['Content-Type'] = 'text/plain'
+    res.write(body)
+    res.finish
+  end
+end
